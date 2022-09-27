05a2f8a13b007015e9f6ba1b6abb6924f1c709d5
Antony Waweru <antonywaweru2002@yahoo.com>
2022-09-27 12:17:48 +0300

    1.1

--- function.js
+++ function.js
@@ -5,3 +5,13 @@ function newElement (){
     list.appendChild(text);
     document.getElementById("listItems").appendChild(list);
 }
+
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
